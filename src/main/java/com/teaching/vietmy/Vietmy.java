190d207dfc74afaa2b4b535b9f3bd5a9d8338575
Luke <npv.loc@gmail.com>
2024-11-01 22:09:47 +0700

    Init

---
+++ src/main/java/com/teaching/vietmy/Vietmy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.teaching.vietmy;
+
+/**
+ *
+ * @author npvlo
+ */
+public class Vietmy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
